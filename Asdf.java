5515d274ecf5d95159743ccdd3b5622214db3113
jonah-walker <jonahlwalker@gmail.com>
2016-11-07 16:46:39 -0500

    upload hello world java program

---
+++ Asdf.java
@@ -0,0 +1,15 @@
+// The "Asdf" class.
+import java.awt.*;
+import hsa.Console;
+
+public class Asdf
+{
+    static Console c;           // The output console
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	System.out.println("Hello World!");
+	// Place your program here.  'c' is the output console
+    } // main method
+} // Asdf class
